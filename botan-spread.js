549253afac1701063f0eea180fb395097ec75530
IMAR ALEXANDER AVILA LOPEZ <iavila@ad.elektra.com.mx>
2017-12-28 18:06:51 -0600

    spread

--- botan-spread.js
+++ botan-spread.js
@@ -7,21 +7,24 @@ module.exports = {
     var book = 'xrp_mxn';
     var max_spread = 0.02;
 
-    var ask_history = [];
-    var bid_history = [];
+    var ask_first = 0;
+    var ask_last = 0;
     var pair_spread = false;
+    var count = 0;
     var op_type = '';
     var op_price = 0;
     var op_date = '';
     var txt_order = '';
-    var count = 0;
-    var profit = 0;
+
+    var op_profit = 0;
+    var op_result = '';
     tick();
 
     function tick() {
-      axios.get('https://api.bitso.com/v3/ticker/?book=' + book)
+      axios.get('https://api.bitso.com/v3/ticker/')
     	.then(function (response) {
-        bot(response.data.payload);
+        console.log('PP', response.data.payload);
+        bot(response.data.payload[3]);
         //var payload = response.data.payload;
         //console.log(' ASK: ', response.data.payload.ask, ' BID: ', response.data.payload.bid, ' DIFF: ', response.data.payload.ask - response.data.payload.bid);
         setTimeout(function() {
@@ -29,7 +32,7 @@ module.exports = {
         }, 500);
     	})
     	.catch(function(error) {
-    		console.log('ERROR');
+    		console.log('ERROR', error);
         setTimeout(function() {
           tick();
         }, 500);
@@ -37,67 +40,70 @@ module.exports = {
     }
     function bot(payload) {
       var spread = payload.ask - payload.bid;
+      console.log((new Date()).getTime() + ' - ' + spread);
       if (spread <= max_spread) {
+        console.log('AF', ask_first, 'AL', ask_last, 'OP', op_type);
         if (pair_spread == false) {
-          if (ask_history.length >= 10) {
-            ask_history.shift();
-            bid_history.shift();
-          }
-          ask_history.push(payload.ask);
-          bid_history.push(payload.bid);
           pair_spread = true;
+          if (ask_first == 0) {
+            ask_first = payload.ask;
+            return;
+          }
+
+          if (ask_first != 0 && ask_last == 0)
+            ask_last = payload.ask;
+
+          if (ask_first > ask_last && op_type == '')
+            op_type = 'BUY';
+          if (ask_first < ask_last && op_type == '')
+            op_type = 'SELL';
+
+          if (op_type == '') {
+            ask_first = ask_last;
+            ask_last = 0;
+            return;
+          }
+          //close buy
+          if (op_type == 'BUY' && op_price != 0) {
+            op_profit = op_price - payload.bid;
+            op_result = profit > 0 ? 'PROFIT' : profit < 0 ? 'LOST' : 'DRAW';
+            txt_order = count + ';' + op_type + ';' + op_date + ';' + op_price + ';' + payload.created_at + ';' + payload.bid + ';' + op_result + ';' + profit;
+            write_order(txt_order);
+            if (op_result == 'PROFIT') {
+                op_type = op_type == 'BUY' ? 'SELL' : 'BUY';
+            }
+          }
+          //close sell
+          if (op_type == 'SELL' && op_price != 0) {
+            op_profit = op_price - payload.ask;
+            op_result = profit > 0 ? 'PROFIT' : profit < 0 ? 'LOST' : 'DRAW';
+            txt_order = count + ';' + op_type + ';' + op_date + ';' + op_price + ';' + payload.created_at + ';' + payload.bid + ';' + op_result + ';' + profit;
+            write_order(txt_order);
+            if (op_result == 'PROFIT') {
+                op_type = op_type == 'BUY' ? 'SELL' : 'BUY';
+            }
+          }
+          //open buy
           if (op_type == 'BUY') {
-            //close buy
-            profit = op_price - payload.bid;
-            txt_order = count + ';' + op_type + ';' + op_date + ';' + op_price + ';' + payload.created_at + ';' + payload.bid + ';' + (profit > 0 ? 'PROFIT' : profit < 0 ? 'LOST' : 'DRAW') + ';' + profit;
-            write_order(txt_order);
-            op_price = 0;
-            op_type = '';
+            op_price = payload.ask;
+            op_date = payload.created_at;
+            count++;
           }
+          //open sell
           if (op_type == 'SELL') {
-            //close sell
-            profit = op_price - payload.ask;
-            txt_order = count + ';' + op_type + ';' + op_date + ';' + op_price + ';' + payload.created_at + ';' + payload.ask + ';' + (profit > 0 ? 'PROFIT' : profit < 0 ? 'LOST' : 'DRAW') + ';' + profit;
-            write_order(txt_order);
-            op_price = 0;
-            op_type = '';
+            op_price = payload.bid;
+            op_date = payload.created_at;
+            count++;
           }
-          if (ask_history.length >= 2) {
-            op_type = get_last_type();
-            if (op_type == 'BUY') {
-              //open buy
-              op_price = payload.ask;
-              op_date = payload.created_at;
-              count++;
-            }
-            if (op_type == 'SELL') {
-              //open sell
-              op_price = payload.bid;
-              op_date = payload.created_at;
-              count++;
-            }
-          }
+
         }
       } else {
         pair_spread = false;
       }
     }
 
-    function get_last_type() {
-      var last_index = ask_history.length - 1;
-      for (var i = last_index; i <= 1; i--) {
-        if (ask_history[i] > ask_history[i - 1]) {
-          return 'SELL';
-        }
-        if (ask_history[i] < ask_history[i - 1]) {
-          return 'BUY';
-        }
-      }
-      return '';
-    }
-
     function write_order(txt) {
-
+      console.log(txt);
       fs.appendFile('orders.log',txt, function(err) {
           if(err) {
               return console.log(err);
